6e9f8fa743d2171ccc22de07f210a9a186e82b30
MEDDOUR Mohammed Nacer Eddine <100869714+Mohamedbeat@users.noreply.github.com>
2023-10-24 22:55:56 +0100

    Add files via upload

---
+++ Index.js
@@ -0,0 +1,29 @@
+// let money ;
+// //get moneyfrom our wallet
+// var wallet = localStorage.getItem("money");
+// if(wallet === undefined || NaN){
+//     // there is no money in our wallet
+//     money = 0
+// }else{
+//     // there is money in our wallet
+//     money = parseInt(wallet)
+// }
+// function showMoney(money){
+//     var message= "$"+money;
+//     document.getElementById('msgContainer').innerHTML = message
+//     // set new money in our wallet
+//     localStorage.setItem("money", parseInt(money));
+// }
+
+// if (
+//   localStorage.key("walet2") === null ||
+//   localStorage.key("walet2") === undefined
+// ) {
+//   localStorage.setItem("walet2", 0);
+// } else {
+//   var money = localStorage.getItem("walet2");
+//   console.log(money);
+//   console.log(localStorage.getItem("walet2"));
+// }
+
+// myRaise = localStorage.getItem("walet2");
